2745e946779a0198c0bd5067b4b63fbdffefad1b
Pedro Santana <pedro.santana9905@gmail.com>
2025-05-07 11:54:18 -0300

    começando o projeto

---
+++ lista-de-produtos/src/components/NavBar/index.jsx
@@ -0,0 +1,11 @@
+
+import './style.css'
+import LogoPrincipal from '../../assets/logo-principal.png'
+
+function NavBar() {
+    <nav>
+        {LogoPrincipal}
+    </nav>
+}
+
+export default NavBar
---
+++ lista-de-produtos/src/pages/Produtos/Produtos.jsx
@@ -0,0 +1,11 @@
+
+import './style.css'
+import NavBar from '../../components/NavBar'
+
+function Produtos () {
+    <>
+        <NavBar/>
+    </>
+}
+
+export default Produtos
